4cb421914e3ae76311c69376b68d141b8a7be4bf
Andresa Martins <andresa@neuralyn.com.br>
2026-01-03 22:10:49 -0300

    Fix CDN url

--- neuralyn-tryon.php
+++ neuralyn-tryon.php
@@ -26,7 +26,7 @@ define( 'NEURALYN_TRYON_PLUGIN_FILE', __FILE__ );
 define( 'NEURALYN_TRYON_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'NEURALYN_TRYON_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'NEURALYN_TRYON_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-define( 'NEURALYN_TRYON_CDN_URL', 'http://localhost:8222' );
+define( 'NEURALYN_TRYON_CDN_URL', 'https://tryon-api.neuralyn.ai' );
 
 /**
  * Check if WooCommerce is active.
