4937c341a2af231fb9985fff232267e81c47d63b
Andresa Martins <andresa@neuralyn.com.br>
2026-01-03 22:19:43 -0300

    Fix button svg

--- templates/button.php
+++ templates/button.php
@@ -12,11 +12,23 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <div class="neuralyn-tryon-button-wrapper">
 	<button type="button" class="neuralyn-tryon-app-button neuralyn-tryon-app-button-default">
-		<span class="neuralyn-tryon-button-icon">
-			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-				<path d="M12 3l1.912 5.813a2 2 0 0 0 1.275 1.275L21 12l-5.813 1.912a2 2 0 0 0-1.275 1.275L12 21l-1.912-5.813a2 2 0 0 0-1.275-1.275L3 12l5.813-1.912a2 2 0 0 0 1.275-1.275L12 3z"/>
-			</svg>
-		</span>
+<svg FILL="currentColor" version="1.0" xmlns="http://www.w3.org/2000/svg"
+    width="15.000000pt" height="10.000000pt" viewBox="0 0 50.000000 54.000000"
+    preserveAspectRatio="xMidYMid meet">
+        <g transform="translate(0.000000,54.000000) scale(0.100000,-0.100000)"
+        stroke="none">
+        <path d="M396 514 c-3 -12 -16 -26 -29 -32 -17 -8 -18 -11 -5 -11 10 -1 24
+        -13 31 -28 14 -25 15 -26 21 -6 4 11 16 25 29 30 l22 10 -21 7 c-12 3 -26 16
+        -31 29 l-10 22 -7 -21z"/>
+        <path d="M193 450 c-21 -87 -60 -126 -158 -156 -24 -7 -34 -14 -25 -17 123
+        -40 159 -70 188 -156 l19 -56 13 45 c19 73 48 110 104 136 28 13 59 24 69 24
+        33 0 17 17 -29 30 -84 24 -144 99 -153 193 -1 4 -4 7 -9 7 -4 0 -13 -23 -19
+        -50z"/>
+        <path d="M416 167 c-3 -14 -19 -31 -37 -39 l-31 -15 27 -7 c17 -4 31 -18 39
+        -38 l13 -30 7 26 c4 17 18 32 37 40 28 12 29 14 11 18 -12 3 -30 20 -40 38
+        l-20 31 -6 -24z"/>
+        </g>
+    </svg>
 		<span class="neuralyn-tryon-button-text"><?php esc_html_e( 'Try-On', 'neuralyn-tryon' ); ?></span>
 	</button>
 </div>
